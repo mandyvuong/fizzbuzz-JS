c0ef65e9207083c769decbc8e2856835834ffb71
Amanda Vuong (she/her) <81902165+mandyvuong@users.noreply.github.com>
2021-09-27 21:40:48 +0100

    divisible by 3

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,19 @@
+describe('Javabuzz', function() {
+
+  let javabuzz;
+
+  describe('knows when a number is', function() {
+
+    it('divisible by 3', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+    });
+  });
+
+  describe('knows when a number is NOT', function() {
+    it('divisible by 3', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(1)).toBe(false)
+    });
+  });
+});
